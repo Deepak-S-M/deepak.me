43d267e504dcbec193b7ded3be762ed4a766bd84
Deepak SM <deepakvnpy@gmail.com>
2024-02-27 19:58:58 +0530

    Professional Added

--- components/Brands/brandData.tsx
+++ components/Brands/brandData.tsx
@@ -3,21 +3,21 @@ import { Brand } from "@/types/brand";
 const brandData: Brand[] = [
   {
     id: 0.25,
-    name: "Client",
+    name: "Full-Stack Developer",
     href: "#",
     image: "/images/brand/brand-light-01.svg",
     imageLight: "/images/brand/brand-dark-01.svg",
   },
   {
     id: 0.3,
-    name: "Client",
+    name: "Game Developer",
     href: "#",
     image: "/images/brand/brand-light-02.svg",
     imageLight: "/images/brand/brand-dark-02.svg",
   },
   {
     id: 0.4,
-    name: "Client",
+    name: "Blogger",
     href: "#",
     image: "/images/brand/brand-light-03.svg",
     imageLight: "/images/brand/brand-dark-03.svg",
