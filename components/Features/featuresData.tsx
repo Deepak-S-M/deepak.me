32d6a673e4332b13bd5c11c5f2d90b95975956b3
Deepak SM <deepakvnpy@gmail.com>
2024-02-28 18:57:47 +0530

    all icons added for academic and career

--- components/Features/featuresData.tsx
+++ components/Features/featuresData.tsx
@@ -3,42 +3,42 @@ import { Feature } from "@/types/feature";
 const featuresData: Feature[] = [
   {
     id: 1,
-    icon: "/images/icon/icon-01.svg",
+    icon: "/images/features/school.svg",
     title: "SSLC",
     description:
       "Completed SSLC at VNUP. Kaleeswari Hr. Secondary School, Vanniyampatty, achieving 94% and graduated in 2016.",
   },
   {
     id: 2,
-    icon: "/images/icon/icon-02.svg",
+    icon: "/images/features/hr-school.svg",
     title: "HSC",
     description:
       "Completed HSC at VNUP. Kaleeswari Hr. Secondary School, Vanniyampatty, achieving 72% and graduated in 2018.",
   },
   {
     id: 3,
-    icon: "/images/features/anjac.png",
+    icon: "/images/features/aj-college.svg",
     title: "UG",
     description:
       "Completed BCA Degree from Ayya Nadar Janakki Ammal College, Sivakasi, achieving 72% and graduated in 2021.",
   },
   {
     id: 4,
-    icon: "/images/features/mepco.jpeg",
+    icon: "/images/features/mepco-college.svg",
     title: "PG",
     description:
       "Completed MCA Degree from Mepco Schlenk Enggineering College, Sivakasi, achieving 74% and graduated in 2023.",
   },
   {
     id: 5,
-    icon: "/images/icon/icon-05.svg",
+    icon: "/images/features/internship.svg",
     title: "Internship",
     description:
       "Completed an internship at EnergizeNow, gaining valuable hands-on experience in software development.",
   },
   {
     id: 6,
-    icon: "/images/icon/icon-03.svg",
+    icon: "/images/features/career.svg",
     title: "Profession",
     description:
       "Currently working as Software Developer at EnergizNow, contributing writing code and debugging software issues, since 2023.",
