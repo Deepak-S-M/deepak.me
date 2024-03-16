d78eb665ab6a5dbc8f57d6d9e124d7b3ad50599b
Deepak SM <121274751+DeepakSMIDC@users.noreply.github.com>
2024-03-16 14:13:59 +0530

    blog data uploaded (#6)
    Co-authored-by: Deepak SM <deepakvnpy@gmail.com>

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -3,10 +3,10 @@ import { Blog } from "@/types/blog";
 const BlogData: Blog[] = [
   {
     _id: 1,
-    mainImage: "/images/blog/blog-01.png",
-    title: "Free advertising for your online business",
+    mainImage: "/images/blog/dsm.png",
+    title: "Dreamweaver Facing Enigmas",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "This series follows the journey of its protagonist as he delves into the enigmatic realms of his dreams. Confronted with puzzling events and surreal landscapes, he navigates through the mysteries that challenge his understanding of reality. Driven by a quest to reunite with his lost love within these dreamscapes, he encounters echoes of memories and embarks on strange adventures. As the story unfolds, it leaves us captivated, eagerly anticipating what twists and turns lie ahead, keeping us hooked until the very end.",
     link: "blog/dsm-ep-01/",
   },
   {
